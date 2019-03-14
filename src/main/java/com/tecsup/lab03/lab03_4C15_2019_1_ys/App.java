058aa77a94b0d48e18a937bc4cd963295490541b
yenifer <yenifer.santiago@tecsup.edu.pe>
2019-03-14 13:23:17 -0500

    first commit

---
+++ src/main/java/com/tecsup/lab03/lab03_4C15_2019_1_ys/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab03.lab03_4C15_2019_1_ys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
